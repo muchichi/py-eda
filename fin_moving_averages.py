de32bdcce939ba48c5a33af32ab7ef8fe4415373
Mussie Nadew <muchichi@gmail.com>
2017-04-15 00:59:29 -0400

    Variable 'google' erros fix

--- fin_moving_averages.py
+++ fin_moving_averages.py
@@ -6,15 +6,15 @@ from matplotlib.dates import date2num
 import pylab
 
 def fin_main():
-    start = datetime.datetime(2017,1,1)
+    start = datetime.datetime(2004,1,1)
     end = datetime.date.today()
     
     google = data.DataReader('GOOG','yahoo',start,end)
     #type(google)
-    plot_data()
+    plot_data(google)
     
-def plot_data():
+def plot_data(df):
     #pylab.rcParams['figure.figsizes'] = (15,9)
-    google['Adj Close'].plot(grid=True)
+    df['Adj Close'].plot(grid=True)
 
 if __name__ == '__main__': fin_main()
